18362cd2966e1603a72590f57ac4bb987620650d
GeoSauer <geoffrey.sauer89@gmail.com>
2022-09-20 13:25:23 -0600

    add display current message

--- app.js
+++ app.js
@@ -3,12 +3,16 @@
 /* Get DOM Elements */
 const trainerHp = document.getElementById('trainer-hp');
 const trainerImage = document.getElementById('trainer-image');
+const resultDisplay = document.getElementById('result-display');
 
 /* State */
 let trainer = {
     hp: 50,
     type: 'trainer',
 };
+
+let result = 'You caught somethin!';
+
 /* Events */
 
 /* Display Functions */
@@ -16,7 +20,14 @@ function displayTrainer() {
     trainerHp.textContent = Math.max(0, trainer.hp);
     if (trainer.hp < 1) {
         trainerImage.src = `assets/ambulance.png`;
+    } else {
+        trainerImage.src = `assets/trainer.png`;
     }
+}
+
+function displayResult() {
+    resultDisplay.textContent = result;
 }
 // (don't forget to call any display functions you want to run on page load!)
 displayTrainer();
+displayResult();
