f19177d91a3bb61595aad0cb4f61bd51438c481e
GeoSauer <geoffrey.sauer89@gmail.com>
2022-09-20 12:47:46 -0600

    add displayTrainer with hp and images for active/fainted

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 /* Imports */
 
 /* Get DOM Elements */
+const trainerHp = document.getElementById('trainer-hp');
+const trainerImage = document.getElementById('trainer-image');
 
 /* State */
-
+let trainer = {
+    hp: 50,
+    type: 'trainer',
+};
 /* Events */
 
 /* Display Functions */
-
+function displayTrainer() {
+    trainerHp.textContent = Math.max(0, trainer.hp);
+    if (trainer.hp < 1) {
+        trainerImage.src = `assets/ambulance.png`;
+    }
+}
 // (don't forget to call any display functions you want to run on page load!)
+displayTrainer();
